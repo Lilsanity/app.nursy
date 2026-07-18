8bbc3e6d617c4549badc09f0c509b544c1ae78fe
Lilian Sejean <sejeanlilian@gmail.com>
2026-07-18 13:28:24 +0200

    Recherche d'infirmières par commune affichée sur la home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
+    if params[:commune].present?
+      commune = Commune.find_by("name ILIKE ?", params[:commune])
+      @commune = commune
+      @nurses = if commune
+        Nurse.search("*",
+          where: { location: { near: { lat: commune.latitude, lon: commune.longitude }, within: "30km" } }
+        )
+      else
+        Nurse.none
+      end
+    end
   end
 end
