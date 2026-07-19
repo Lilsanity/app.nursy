479fee1c8b2604a40023030a6f26aee6c02c2bd5
Pamela Morganti <pahelemoba@gmail.com>
2026-07-19 15:30:33 +0200

    implement my_space view with dynamic calendar and appointment list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,13 @@
 class PagesController < ApplicationController
   def home
     if params[:commune].present?
-      commune = Commune.find_by("name ILIKE ?", params[:commune])
-      @commune = commune
-      @nurses = if commune
-        Nurse.search("*",
-          where: { location: { near: { lat: commune.latitude, lon: commune.longitude }, within: "30km" } }
-        )
-      else
-        Nurse.none
-      end
+      @commune = Commune.find_by("name ILIKE ?", params[:commune])
+      @nurses = @commune ? Nurse.joins(:commune).where(communes: { id: @commune.id }) : Nurse.none
     end
+  end
+
+  def my_space
+    @user = current_user
+    @appointments = @user.appointments.includes(:nurse).order(created_at: :desc)
   end
 end
