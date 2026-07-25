45a9dcf93591a9347b1403af6ffb2b639508f92e
farzana abul kalam azad <bfarzane@hotmail.com>
2026-07-25 23:47:21 +0200

    Ajout upload photo de profil dans Mon Espace

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,19 @@
 class PagesController < ApplicationController
   def home
-    if params[:commune].present?
-      @commune = Commune.find_by("name ILIKE ?", params[:commune])
-      @nurses = @commune ? Nurse.joins(:commune).where(communes: { id: @commune.id }) : Nurse.none
-    end
+    return unless params[:commune].present?
+
+    @commune = Commune.find_by("name ILIKE ?", params[:commune])
+    @nurses = @commune ? Nurse.joins(:commune).where(communes: { id: @commune.id }) : Nurse.none
   end
 
   def my_space
     @user = current_user
     @appointments = @user.appointments.includes(:nurse, :availability).order(created_at: :desc)
+  end
+
+  def update_photo
+    current_user.photo.attach(params[:photo])
+    redirect_to my_space_path
   end
 
   def confirmation
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
